ef58f2bf2ff015530b96351118b7b84936627fad
Sophia Mahnke <Sophia@Gideon.home>
2016-11-14 21:33:48 +0100

    LPTHW exercises 38 - 44

---
+++ EX45/ex45.py
@@ -0,0 +1,15 @@
+# super cool game
+
+
+
+
+
+
+
+
+def start():
+    "Hey there! Whats your name?"
+    raw_input(">_") = name #make it blink!
+    "\n Okay, %s. So today you're going to" % name
+
+start()
